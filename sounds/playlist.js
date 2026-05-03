546ef421665adceb20a0e440ba50019c581a3744
Jun Jie Yam <yjjcoolcool@gmail.com>
2026-05-03 21:15:19 +0800

    Add more random cabin noises

--- sounds/playlist.js
+++ sounds/playlist.js
@@ -2,4 +2,6 @@ window.TRAIN_SOUND_EFFECTS = [
   { src: "sounds/noise_1.ogg", volume: 1 },
   { src: "sounds/noise_2.ogg", volume: 1 },
   { src: "sounds/noise_3.ogg", volume: 1 },
+  { src: "sounds/noise_4.ogg", volume: 1 },
+  { src: "sounds/noise_5.ogg", volume: 1 },
 ];
